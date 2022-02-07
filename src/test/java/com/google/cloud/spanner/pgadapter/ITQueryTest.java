c37d2e4b38cfb14d24721b4ced3c96c783ae3d21
Knut Olav Løite <koloite@gmail.com>
2022-02-07 09:04:53 +0100

    fix: CI integration tests ignored environment variable values (#31)
    * fix: CI integration tests ignored environment variable values
    
    The CI integration tests failed for a couple of reasons:
    1. The spanner-testing instance is located in us-central1 which is no
       longer allow-listed for creating PG databases.
    2. The staging endpoint currently returns an INVALID_ARGUMENT error for
       all CreateDatabase requests.
    3. Changing the values for the above two in the environment variables
       for the CI script did not have any effect, as the Maven command
       executing the tests did not take these into account.
    
    * fix: typo in mvn command + expected tables
    
    * fix: remove table that is not yet available on prod
    
    * fix: also change the row count
    
    * fix: let the OS determine a random available port
    
    The integration tests would assign a random port for PgAdapter to use if
    no port was specified. This was however just a random number, and there
    was no guarantee that that port was not already used by anything else on
    the host machine. This could cause random build failures.
    
    * fix: remove hardcoded options

--- src/test/java/com/google/cloud/spanner/pgadapter/ITQueryTest.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/ITQueryTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertThat;
 
 import com.google.cloud.spanner.Database;
 import com.google.cloud.spanner.pgadapter.metadata.OptionsMetadata;
+import com.google.common.collect.ImmutableList;
 import com.google.common.primitives.Bytes;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
@@ -66,21 +67,24 @@ public final class ITQueryTest implements IntegrationTest {
     Database db = testEnv.createDatabase();
     testEnv.updateDdl(db.getId().getDatabase(), Arrays.asList(ddl));
     testEnv.updateTables(db.getId().getDatabase(), Arrays.asList(dml));
-    args =
-        new String[] {
-          "-p",
-          testEnv.getProjectId(),
-          "-i",
-          testEnv.getInstanceId(),
-          "-d",
-          db.getId().getDatabase(),
-          "-c",
-          testEnv.getCredentials(),
-          "-s",
-          String.valueOf(testEnv.getPort()),
-          "-e",
-          testEnv.getUrl().getHost()
-        };
+    String credentials = testEnv.getCredentials();
+    ImmutableList.Builder<String> argsListBuilder =
+        ImmutableList.<String>builder()
+            .add(
+                "-p",
+                testEnv.getProjectId(),
+                "-i",
+                testEnv.getInstanceId(),
+                "-d",
+                db.getId().getDatabase(),
+                "-s",
+                String.valueOf(testEnv.getPort()),
+                "-e",
+                testEnv.getUrl().getHost());
+    if (credentials != null) {
+      argsListBuilder.add("-c", testEnv.getCredentials());
+    }
+    args = argsListBuilder.build().toArray(new String[0]);
   }
 
   @Before
@@ -103,7 +107,7 @@ public final class ITQueryTest implements IntegrationTest {
   public void simplePgQuery() throws Exception {
     testEnv.waitForServer(server);
 
-    Socket clientSocket = new Socket(InetAddress.getByName(null), testEnv.getPort());
+    Socket clientSocket = new Socket(InetAddress.getByName(null), server.getLocalPort());
     DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
     DataInputStream in = new DataInputStream(clientSocket.getInputStream());
     testEnv.initializeConnection(out);
@@ -138,7 +142,7 @@ public final class ITQueryTest implements IntegrationTest {
   public void basicSelectTest() throws Exception {
     testEnv.waitForServer(server);
 
-    Socket clientSocket = new Socket(InetAddress.getByName(null), testEnv.getPort());
+    Socket clientSocket = new Socket(InetAddress.getByName(null), server.getLocalPort());
     DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
     DataInputStream in = new DataInputStream(clientSocket.getInputStream());
     testEnv.initializeConnection(out);
