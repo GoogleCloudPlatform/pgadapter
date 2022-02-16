7969a339139645e857d641526ee8127d745fda23
Knut Olav Løite <koloite@gmail.com>
2022-02-16 15:17:04 +0100

    test: add more tests

--- src/test/java/com/google/cloud/spanner/pgadapter/JdbcMockServerTest.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/JdbcMockServerTest.java
@@ -18,13 +18,32 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.google.cloud.ByteArray;
+import com.google.cloud.Timestamp;
+import com.google.cloud.spanner.MockSpannerServiceImpl.StatementResult;
+import com.google.cloud.spanner.Statement;
+import com.google.protobuf.ListValue;
+import com.google.protobuf.Value;
 import com.google.spanner.v1.ExecuteBatchDmlRequest;
 import com.google.spanner.v1.ExecuteSqlRequest;
+import com.google.spanner.v1.ResultSetMetadata;
+import com.google.spanner.v1.StructType;
+import com.google.spanner.v1.StructType.Field;
+import com.google.spanner.v1.Type;
+import com.google.spanner.v1.TypeAnnotationCode;
+import com.google.spanner.v1.TypeCode;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
+import org.bouncycastle.util.encoders.Base64;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +51,63 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class JdbcMockServerTest extends AbstractMockServerTest {
+  private static final ResultSetMetadata ALL_TYPES_METADATA =
+      ResultSetMetadata.newBuilder()
+          .setRowType(
+              StructType.newBuilder()
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_bigint")
+                          .setType(Type.newBuilder().setCode(TypeCode.INT64).build()))
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_bool")
+                          .setType(Type.newBuilder().setCode(TypeCode.BOOL).build()))
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_bytea")
+                          .setType(Type.newBuilder().setCode(TypeCode.BYTES).build()))
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_float8")
+                          .setType(Type.newBuilder().setCode(TypeCode.FLOAT64).build()))
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_numeric")
+                          .setType(
+                              Type.newBuilder()
+                                  .setCode(TypeCode.NUMERIC)
+                                  .setTypeAnnotation(TypeAnnotationCode.PG_NUMERIC)
+                                  .build()))
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_timestamptz")
+                          .setType(Type.newBuilder().setCode(TypeCode.TIMESTAMP).build()))
+                  .addFields(
+                      Field.newBuilder()
+                          .setName("col_varchar")
+                          .setType(Type.newBuilder().setCode(TypeCode.STRING).build()))
+                  .build())
+          .build();
+  private static final com.google.spanner.v1.ResultSet ALL_TYPES_RESULTSET =
+      com.google.spanner.v1.ResultSet.newBuilder()
+          .setMetadata(ALL_TYPES_METADATA)
+          .addRows(
+              ListValue.newBuilder()
+                  .addValues(Value.newBuilder().setStringValue("1").build())
+                  .addValues(Value.newBuilder().setBoolValue(true).build())
+                  .addValues(
+                      Value.newBuilder()
+                          .setStringValue(
+                              Base64.toBase64String("test".getBytes(StandardCharsets.UTF_8)))
+                          .build())
+                  .addValues(Value.newBuilder().setNumberValue(3.14d).build())
+                  .addValues(Value.newBuilder().setStringValue("6.626").build())
+                  .addValues(
+                      Value.newBuilder().setStringValue("2022-02-16T13:18:02.123456789Z").build())
+                  .addValues(Value.newBuilder().setStringValue("test").build())
+                  .build())
+          .build();
 
   @BeforeClass
   public static void loadPgJdbcDriver() throws Exception {
@@ -54,6 +130,68 @@ public class JdbcMockServerTest extends AbstractMockServerTest {
         assertTrue(resultSet.next());
         assertEquals(1L, resultSet.getLong(1));
         assertFalse(resultSet.next());
+      }
+    }
+  }
+
+  @Test
+  public void testQueryWithParameters() throws SQLException {
+    String jdbcSql =
+        "select col_bigint, col_bool, col_bytea, col_float8, col_numeric, col_timestamptz, col_varchar "
+            + "from all_types "
+            + "where col_bigint=? "
+            + "and col_bool=? "
+            + "and col_bytea=? "
+            + "and col_float8=? "
+            //        + "and col_numeric=? "
+            + "and col_timestamptz=? "
+            + "and col_varchar=?";
+    String pgSql =
+        "select col_bigint, col_bool, col_bytea, col_float8, col_numeric, col_timestamptz, col_varchar "
+            + "from all_types "
+            + "where col_bigint=$1 "
+            + "and col_bool=$2 "
+            + "and col_bytea=$3 "
+            + "and col_float8=$4 "
+            //        + "and col_numeric=$5 "
+            + "and col_timestamptz=$5 "
+            + "and col_varchar=$6";
+    mockSpanner.putStatementResult(StatementResult.query(Statement.of(pgSql), ALL_TYPES_RESULTSET));
+    mockSpanner.putStatementResult(
+        StatementResult.query(
+            Statement.newBuilder(pgSql)
+                .bind("p1")
+                .to(1L)
+                .bind("p2")
+                .to(true)
+                .bind("p3")
+                .to(ByteArray.copyFrom("test"))
+                .bind("p4")
+                .to(3.14d)
+                // .bind("p5").to(com.google.cloud.spanner.Value.pgNumeric("6.626"))
+                .bind("p5")
+                .to(Timestamp.parseTimestamp("2022-02-16T13:18:02.123457000Z"))
+                .bind("p6")
+                .to("test")
+                .build(),
+            ALL_TYPES_RESULTSET));
+
+    OffsetDateTime zonedDateTime =
+        LocalDateTime.of(2022, 2, 16, 13, 18, 2, 123456789).atOffset(ZoneOffset.UTC);
+    try (Connection connection = DriverManager.getConnection(createUrl())) {
+      try (PreparedStatement preparedStatement = connection.prepareStatement(jdbcSql)) {
+        preparedStatement.setLong(1, 1L);
+        preparedStatement.setBoolean(2, true);
+        preparedStatement.setBytes(3, "test".getBytes(StandardCharsets.UTF_8));
+        preparedStatement.setDouble(4, 3.14d);
+        // preparedStatement.setBigDecimal(5, new BigDecimal("6.626"));
+        preparedStatement.setTimestamp(5, java.sql.Timestamp.from(Instant.from(zonedDateTime)));
+        preparedStatement.setString(6, "test");
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+          assertTrue(resultSet.next());
+          assertEquals(1L, resultSet.getLong(1));
+          assertFalse(resultSet.next());
+        }
       }
     }
   }
