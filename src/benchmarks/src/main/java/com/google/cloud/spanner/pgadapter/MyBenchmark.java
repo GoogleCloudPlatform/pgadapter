553288147cbf46e77005dc0c6bde1a45977493a8
Knut Olav Løite <koloite@gmail.com>
2022-03-13 19:25:23 +0100

    perf: add benchmarks

---
+++ src/benchmarks/src/main/java/com/google/cloud/spanner/pgadapter/MyBenchmark.java
@@ -0,0 +1,14 @@
+
+package com.google.cloud.spanner.pgadapter;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class MyBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+
+}
