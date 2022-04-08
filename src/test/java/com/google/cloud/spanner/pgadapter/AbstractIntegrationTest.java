d67632e9032b4d629edd3e1647e5a76ad67ea3ba
Knut Olav Løite <koloite@gmail.com>
2022-04-08 17:57:26 +0200

    test: run integration tests against Docker build

---
+++ src/test/java/com/google/cloud/spanner/pgadapter/AbstractIntegrationTest.java
@@ -0,0 +1,72 @@
+package com.google.cloud.spanner.pgadapter;
+
+import com.google.cloud.spanner.Database;
+import com.google.cloud.spanner.pgadapter.metadata.OptionsMetadata;
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@Category(IntegrationTest.class)
+@RunWith(JUnit4.class)
+public class AbstractIntegrationTest {
+  private static final PgAdapterTestEnv testEnv = new PgAdapterTestEnv();
+  private static final String
+  private static ProxyServer server;
+  private static Database database;
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    testEnv.setUp();
+    if (testEnv.isUseExistingDb()) {
+      database = testEnv.getExistingDatabase();
+    } else {
+      database = testEnv.createDatabase();
+      testEnv.updateDdl(
+          database.getId().getDatabase(),
+          Arrays.asList(
+              "create table numbers (num int not null primary key, name varchar(100))",
+              "create table all_types ("
+                  + "col_bigint bigint not null primary key, "
+                  + "col_bool bool, "
+                  + "col_bytea bytea, "
+                  + "col_float8 float8, "
+                  + "col_int int, "
+                  + "col_numeric numeric, "
+                  + "col_timestamptz timestamptz, "
+                  + "col_varchar varchar(100))"));
+    }
+    String credentials = testEnv.getCredentials();
+    ImmutableList.Builder<String> argsListBuilder =
+        ImmutableList.<String>builder()
+            .add(
+                "-p",
+                testEnv.getProjectId(),
+                "-i",
+                testEnv.getInstanceId(),
+                "-d",
+                database.getId().getDatabase(),
+                "-s",
+                String.valueOf(testEnv.getPort()),
+                "-e",
+                testEnv.getUrl().getHost());
+    if (credentials != null) {
+      argsListBuilder.add("-c", testEnv.getCredentials());
+    }
+    String[] args = argsListBuilder.build().toArray(new String[0]);
+    server = new ProxyServer(new OptionsMetadata(args));
+    server.startServer();
+  }
+
+  @AfterClass
+  public static void teardown() {
+    if (server != null) {
+      server.stopServer();
+    }
+    testEnv.cleanUp();
+  }
+
+}
