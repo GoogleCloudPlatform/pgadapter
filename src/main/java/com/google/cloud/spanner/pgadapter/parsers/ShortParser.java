f8f1dacdb2ef7ea289757eb1658a287aa8fc5b51
Knut Olav Løite <koloite@gmail.com>
2022-04-19 16:23:05 +0200

    feat: support more JDBC parameter types
    Adds support for int2 and float4 parameters.
    Adds tests for encoding all supported parameters types in both text and
    binary format (where the JDBC driver allows it).

---
+++ src/main/java/com/google/cloud/spanner/pgadapter/parsers/ShortParser.java
@@ -0,0 +1,64 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.spanner.pgadapter.parsers;
+
+import com.google.cloud.spanner.Statement;
+import java.sql.Types;
+import org.postgresql.util.ByteConverter;
+
+/** Translate from wire protocol to short. */
+class ShortParser extends Parser<Short> {
+
+  ShortParser(byte[] item, FormatCode formatCode) {
+    if (item != null) {
+      switch (formatCode) {
+        case TEXT:
+          this.item = Short.valueOf(new String(item));
+          break;
+        case BINARY:
+          this.item = ByteConverter.int2(item, 0);
+          break;
+        default:
+          throw new IllegalArgumentException("Unsupported format: " + formatCode);
+      }
+    }
+  }
+
+  @Override
+  public int getSqlType() {
+    return Types.SMALLINT;
+  }
+
+  @Override
+  protected String stringParse() {
+    return this.item == null ? null : Short.toString(this.item);
+  }
+
+  @Override
+  protected byte[] binaryParse() {
+    return this.item == null ? null : binaryParse(this.item);
+  }
+
+  public static byte[] binaryParse(int value) {
+    byte[] result = new byte[2];
+    ByteConverter.int2(result, 0, value);
+    return result;
+  }
+
+  @Override
+  public void bind(Statement.Builder statementBuilder, String name) {
+    statementBuilder.bind(name).to(this.item == null ? null : this.item.longValue());
+  }
+}
