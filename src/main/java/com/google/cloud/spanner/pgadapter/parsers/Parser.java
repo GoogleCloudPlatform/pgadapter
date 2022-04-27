21a7bd0a3e2d7df77270a83cec3a8bca5c321a15
Knut Olav Løite <koloite@gmail.com>
2022-04-27 09:20:06 +0200

    feat: support more JDBC parameter types (#118)
    Adds support for int2 and float4 parameters.
    Adds tests for encoding all supported parameters types in both text and
    binary format (where the JDBC driver allows it).

--- src/main/java/com/google/cloud/spanner/pgadapter/parsers/Parser.java
+++ src/main/java/com/google/cloud/spanner/pgadapter/parsers/Parser.java
@@ -95,12 +95,16 @@ public abstract class Parser<T> {
         return new BinaryParser(item, formatCode);
       case Oid.DATE:
         return new DateParser(item, formatCode);
+      case Oid.FLOAT4:
+        return new FloatParser(item, formatCode);
       case Oid.FLOAT8:
         return new DoubleParser(item, formatCode);
-      case Oid.INT8:
-        return new LongParser(item, formatCode);
+      case Oid.INT2:
+        return new ShortParser(item, formatCode);
       case Oid.INT4:
         return new IntegerParser(item, formatCode);
+      case Oid.INT8:
+        return new LongParser(item, formatCode);
       case Oid.NUMERIC:
         return new NumericParser(item, formatCode);
       case Oid.TEXT:
@@ -118,7 +122,7 @@ public abstract class Parser<T> {
         }
         return create(item, type, formatCode);
       default:
-        throw new IllegalArgumentException("Illegal or unknown element type: " + oidType);
+        throw new IllegalArgumentException("Unsupported parameter type: " + oidType);
     }
   }
 
