91610b2a4f49f04c4b6d22a2ee8e38e7fd7a97fa
gauravsnj <gauravsnj@google.com>
2022-06-10 15:37:22 +0530

    added code

---
+++ src/test/python/StatementsWithoutParameters.py
@@ -0,0 +1,76 @@
+''' Copyright 2022 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+'''
+
+import psycopg2 as pg
+import sys
+
+
+def create_connection(port):
+  try:
+    connection = pg.connect(user="postgres",
+                            database="postgres",
+                            host="localhost",
+                            port=port)
+    connection.autocommit = True
+    return connection
+  except Exception as e:
+    print(e)
+    return None
+
+
+def execute_query(sql, port):
+  connection = create_connection(port)
+  if connection is None:
+    return
+  try:
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    for row in cursor:
+      print(row)
+  except Exception as e:
+    print(e)
+  finally:
+    cursor.close()
+    connection.close()
+
+
+def execute_update(sql, port):
+  connection = create_connection(port)
+  if connection == None:
+    return
+  try:
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    print(cursor.rowcount)
+  except Exception as e:
+    print(e)
+  finally:
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+  assert len(sys.argv) == 4
+
+  port = sys.argv[1]
+  statement_type = sys.argv[2]
+  sql = sys.argv[3]
+
+  if statement_type == 'query':
+    execute_query(sql, port)
+  elif statement_type == 'update':
+    execute_update(sql, port)
+  else:
+    print('Invalid Statement Type')
