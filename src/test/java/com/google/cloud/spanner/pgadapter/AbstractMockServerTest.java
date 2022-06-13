44a4bf140fb9033c3fb1a5ac051473517e7e6d13
Knut Olav Løite <koloite@gmail.com>
2022-06-13 18:03:44 +0200

    test: remove deprecated arguments for tests (#181)
    Remove the deprecated arguments for specific clients in tests, as
    PGAdapter can automatically detect which client has connected.

--- src/test/java/com/google/cloud/spanner/pgadapter/AbstractMockServerTest.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/AbstractMockServerTest.java
@@ -351,7 +351,6 @@ public abstract class AbstractMockServerTest {
       argsListBuilder.add("-d", defaultDatabase);
     }
     argsListBuilder.add(
-        "-jdbc",
         "-debug",
         "-c",
         "", // empty credentials file, as we are using a plain text connection.
