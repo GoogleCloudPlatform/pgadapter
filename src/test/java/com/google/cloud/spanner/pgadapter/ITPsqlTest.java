194cd4112730389689d2590134e80519a85ef6b9
Knut Olav Løite <koloite@gmail.com>
2022-07-07 14:55:28 +0200

    test: fix integration tests

--- src/test/java/com/google/cloud/spanner/pgadapter/ITPsqlTest.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/ITPsqlTest.java
@@ -138,7 +138,9 @@ public class ITPsqlTest implements IntegrationTest {
           String.join(";", DEFAULT_DATA_MODEL) + ";\n"
         };
     ProcessBuilder builder = new ProcessBuilder().command(createTablesCommand);
-    builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    if (POSTGRES_PASSWORD != null) {
+      builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    }
     Process process = builder.start();
 
     int res = process.waitFor();
@@ -161,7 +163,9 @@ public class ITPsqlTest implements IntegrationTest {
           "drop table if exists all_types; drop table if exists numbers;"
         };
     ProcessBuilder builder = new ProcessBuilder().command(dropTablesCommand);
-    builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    if (POSTGRES_PASSWORD != null) {
+      builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    }
     Process process = builder.start();
 
     int res = process.waitFor();
@@ -209,7 +213,9 @@ public class ITPsqlTest implements IntegrationTest {
           POSTGRES_DATABASE
         };
     builder.command(psqlCommand);
-    builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    if (POSTGRES_PASSWORD != null) {
+      builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    }
     Process process = builder.start();
     String errors;
     String output;
@@ -273,7 +279,9 @@ public class ITPsqlTest implements IntegrationTest {
             + " -d "
             + database.getId().getDatabase()
             + " -c \"copy all_types from stdin;\"\n");
-    builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    if (POSTGRES_PASSWORD != null) {
+      builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    }
     Process process = builder.start();
     int res = process.waitFor();
     assertEquals(0, res);
@@ -378,7 +386,9 @@ public class ITPsqlTest implements IntegrationTest {
             + " -d "
             + POSTGRES_DATABASE
             + " -c \"copy all_types from stdin;\"\n");
-    builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    if (POSTGRES_PASSWORD != null) {
+      builder.environment().put("PGPASSWORD", POSTGRES_PASSWORD);
+    }
     Process process = builder.start();
     int res = process.waitFor();
     assertEquals(0, res);
