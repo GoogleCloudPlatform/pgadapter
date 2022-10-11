1137ed16725e7735da72573124244e05503bb19e
gauravsnj <98738378+gauravsnj@users.noreply.github.com>
2022-10-11 18:20:39 +0530

    feat: added support for Django specific statement (#382)
    * added the code for the Django specific statement
    
    * added tests
    
    * formatted the files
    
    * changed the file name
    
    * Update DjangoSpecificStatementTest.java
    
    * Update DjangoGetTableNamesTest.java
    
    * Update DjangoGetTableNamesTest.java
    
    * Update DjangoGetTableNamesStatement.java

--- src/main/java/com/google/cloud/spanner/pgadapter/utils/ClientAutoDetector.java
+++ src/main/java/com/google/cloud/spanner/pgadapter/utils/ClientAutoDetector.java
@@ -16,6 +16,7 @@ package com.google.cloud.spanner.pgadapter.utils;
 
 import com.google.api.core.InternalApi;
 import com.google.cloud.spanner.pgadapter.ConnectionHandler;
+import com.google.cloud.spanner.pgadapter.statements.local.DjangoGetTableNamesStatement;
 import com.google.cloud.spanner.pgadapter.statements.local.ListDatabasesStatement;
 import com.google.cloud.spanner.pgadapter.statements.local.LocalStatement;
 import com.google.cloud.spanner.pgadapter.statements.local.SelectCurrentCatalogStatement;
@@ -39,7 +40,8 @@ public class ClientAutoDetector {
       ImmutableList.of(
           SelectCurrentSchemaStatement.INSTANCE,
           SelectCurrentDatabaseStatement.INSTANCE,
-          SelectCurrentCatalogStatement.INSTANCE);
+          SelectCurrentCatalogStatement.INSTANCE,
+          DjangoGetTableNamesStatement.INSTANCE);
 
   public enum WellKnownClient {
     PSQL {
