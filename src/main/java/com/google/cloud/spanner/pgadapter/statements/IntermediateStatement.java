ff01c8bb43730a85f7b7da8bfe04e41aa6357af2
Knut Olav Løite <koloite@gmail.com>
2022-10-14 15:20:06 +0200

    test: log error

--- src/main/java/com/google/cloud/spanner/pgadapter/statements/IntermediateStatement.java
+++ src/main/java/com/google/cloud/spanner/pgadapter/statements/IntermediateStatement.java
@@ -218,7 +218,10 @@ public class IntermediateStatement {
     if (this.futureStatementResult != null) {
       if (resultNotReadyBehavior == ResultNotReadyBehavior.FAIL
           && !this.futureStatementResult.isDone()) {
-        throw new IllegalStateException("Statement result cannot be retrieved before flush/sync");
+        IllegalStateException e =
+            new IllegalStateException("Statement result cannot be retrieved before flush/sync");
+        e.printStackTrace();
+        throw e;
       }
       try {
         setStatementResult(this.futureStatementResult.get());
