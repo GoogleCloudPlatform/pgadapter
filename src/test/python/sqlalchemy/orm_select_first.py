2bb0ff5ba022da5a6088fcb4e6ff6b99421560c4
Knut Olav Løite <koloite@gmail.com>
2022-11-25 18:51:39 +0100

    fix: always use UTC

--- src/test/python/sqlalchemy/orm_select_first.py
+++ src/test/python/sqlalchemy/orm_select_first.py
@@ -4,7 +4,7 @@ from connect import create_test_engine
 from all_types import AllTypes
 
 
-engine = create_test_engine()
+engine = create_test_engine(options="?options=-c timezone=UTC")
 session = Session(engine)
 row = session.scalars(select(AllTypes)).first()
 print(row)
