a65fe6bf883c43f95df03a8c79da7ab394628496
gauravsnj <gauravsnj@google.com>
2022-12-09 12:54:02 +0530

    formatted file lint

--- src/test/java/com/google/cloud/spanner/pgadapter/python/django/DjangoTestSetup.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/python/django/DjangoTestSetup.java
@@ -196,7 +196,6 @@ public class DjangoTestSetup extends DjangoMockServerTest {
     return execute(port, host, options, "transaction_test.py");
   }
 
-
   public String executePgAggregateTests(int port, String host, List<String> options)
       throws IOException, InterruptedException {
     return execute(port, host, options, "pg_aggregates_test.py");
@@ -205,7 +204,6 @@ public class DjangoTestSetup extends DjangoMockServerTest {
   public String executeConditionalTests(int port, String host, List<String> options)
       throws IOException, InterruptedException {
     return execute(port, host, options, "conditional_expressions_test.py");
-
   }
 
   public String executeAggregationTests(int port, String host, List<String> options)
