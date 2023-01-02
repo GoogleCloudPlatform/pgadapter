ceb961244b1a1ee420d294c930c48a6e0ac179bd
Knut Olav Løite <koloite@gmail.com>
2023-01-02 10:28:03 +0100

    feat: read ycsb results for dashboard

--- src/test/java/com/google/cloud/spanner/pgadapter/DashboardGenerator.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/DashboardGenerator.java
@@ -14,9 +14,19 @@
 
 package com.google.cloud.spanner.pgadapter;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.Statement;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -41,4 +51,26 @@ public class DashboardGenerator {
       writer.flush();
     }
   }
+
+  private static void readYcsbResults() throws IOException {
+    try (Spanner spanner = SpannerOptions.newBuilder()
+        .setCredentials(GoogleCredentials.fromStream(
+            Files.newInputStream(Paths.get("/Users/loite/Downloads/cloud-spanner-pg-adapter.json"))))
+        .build().getService()) {
+      DatabaseClient client = spanner.getDatabaseClient(
+          DatabaseId.of(
+              spanner.getOptions().getProjectId(),
+              "pgadapter-ycsb-regional-test",
+              "pgadapter-ycsb-test"
+              ));
+      String lastExecutionTime;
+      try (ResultSet resultSet = client.singleUse().executeQuery(Statement.of("select max(executed_at) from run"))) {
+        while (resultSet.next()) {
+          lastExecutionTime = resultSet.getString(0);
+        }
+      }
+      
+    }
+  }
+
 }
