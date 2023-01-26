a0b00d6b80041f92830986455b8390385b0f05ad
Knut Olav Løite <koloite@gmail.com>
2023-01-26 14:50:26 +0100

    test: add tests for timezone 'EST' (#600)
    * test: add tests for timezone 'EST'
    
    Fixes #598
    
    * fix: skip unknown timezone id

--- src/test/java/com/google/cloud/spanner/pgadapter/ITPsqlTest.java
+++ src/test/java/com/google/cloud/spanner/pgadapter/ITPsqlTest.java
@@ -47,6 +47,7 @@ import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.zone.ZoneRulesException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
@@ -668,9 +669,13 @@ public class ITPsqlTest implements IntegrationTest {
                           "select name from pg_timezone_names where not name like '%%posix%%' and not name like 'Factory' offset %d limit 1",
                           random.nextInt(numTimezones)))) {
             assertTrue(resultSet.next());
-            timezone = resultSet.getString(1);
-            if (!PROBLEMATIC_ZONE_IDS.contains(ZoneId.of(timezone))) {
-              break;
+            try {
+              timezone = resultSet.getString(1);
+              if (!PROBLEMATIC_ZONE_IDS.contains(ZoneId.of(timezone))) {
+                break;
+              }
+            } catch (ZoneRulesException ignore) {
+              // Skip and try a different one if it is not a supported timezone on this system.
             }
           }
         }
