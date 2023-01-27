90027e797fb7365478bcb24476f24e396a34eadb
Knut Olav Løite <koloite@gmail.com>
2023-01-27 15:28:41 +0100

    chore: remove unnecessary change

--- src/main/java/com/google/cloud/spanner/pgadapter/statements/CopyStatement.java
+++ src/main/java/com/google/cloud/spanner/pgadapter/statements/CopyStatement.java
@@ -563,7 +563,7 @@ public class CopyStatement extends IntermediatePortalStatement {
     }
     ParsedCopyStatement.Builder builder = new ParsedCopyStatement.Builder();
     if (parser.eatToken("(")) {
-      builder.query = parser.parseExpressionUntilKeyword(ImmutableList.of("to"), true, true, false);
+      builder.query = parser.parseExpressionUntilKeyword(ImmutableList.of(), true, true, false);
       if (!parser.eatToken(")")) {
         throw PGExceptionFactory.newPGException(
             "missing closing parentheses after query", SQLState.SyntaxError);
