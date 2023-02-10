16d2d1fba6cbe3775e7be011a2efe3e848529a67
Knut Olav Løite <koloite@gmail.com>
2023-02-10 17:00:43 +0100

    test: add sample tests

--- samples/python/sqlalchemy2-sample/connect.py
+++ samples/python/sqlalchemy2-sample/connect.py
@@ -1,4 +1,4 @@
-""" Copyright 2022 Google LLC
+""" Copyright 2023 Google LLC
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -17,21 +17,27 @@ import argparse
 from sqlalchemy import create_engine
 
 
-def create_test_engine(autocommit=False, options=""):
-  parser = argparse.ArgumentParser(description='Run SQLAlchemy tests.')
+# Creates a database engine that can be used with the sample.py script in this
+# directory. This function assumes that the host, port and database name was
+# given as command line arguments.
+def create_test_engine(autocommit: bool = False, options: str = ""):
+  parser = argparse.ArgumentParser(description='Run SQLAlchemy sample.')
   parser.add_argument('host', type=str, help='host to connect to')
   parser.add_argument('port', type=int, help='port number to connect to')
-  parser.add_argument('database', type=str, help='database to connect to')
+  parser.add_argument('database', type=str, help='database to connect to', default='d')
   args = parser.parse_args()
 
-  conn_string = "postgresql+psycopg://user:password@{host}:{port}/d{options}".format(
-    host=args.host, port=args.port, options=options)
+  conn_string = "postgresql+psycopg://user:password@{host}:{port}/" \
+                "{database}{options}".format(host=args.host,
+                                             port=args.port,
+                                             database=args.database,
+                                             options=options)
   if args.host == "":
     if options == "":
       conn_string = conn_string + "?host=/tmp"
     else:
       conn_string = conn_string + "&host=/tmp"
-  conn = create_engine(conn_string, future=True)
+  engine = create_engine(conn_string, future=True)
   if autocommit:
-    return conn.execution_options(isolation_level="AUTOCOMMIT")
-  return conn
+    engine = engine.execution_options(isolation_level="AUTOCOMMIT")
+  return engine
