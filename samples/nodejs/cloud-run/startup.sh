9d84c35dd7bf710e8c781ad5ec2c26e80e25be48
Knut Olav Løite <koloite@gmail.com>
2023-06-16 14:49:02 +0200

    docs: add sample for Node.js with Cloud Run

---
+++ samples/nodejs/cloud-run/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run both PGAdapter and the web server when the Docker container is started.
+# Start PGAdapter in plain TCP only mode, that is:
+# 1. Do not specify any credentials. This will make sure PGAdapter picks the credentials from the current environment.
+#    You must ensure that the service account that is used by Cloud Run is allowed to access the Cloud Spanner database
+#    that your application is using.
+# 2. Do not specify any specific project, instance or database. This will require the application to specify a fully
+#    qualified database name when connecting to PGAdapter.
+# 3. Set the Unix domain socket directory to the empty string to disable domain sockets. Domain sockets are not
+#    supported on Cloud Run.
+
+# Start PGAdapter in the background.
+java -jar /pgadapter/pgadapter.jar -dir= &
+# Give PGAdapter a second to start up.
+sleep 1
+
+# Run the web service on container startup.
+node index.js
