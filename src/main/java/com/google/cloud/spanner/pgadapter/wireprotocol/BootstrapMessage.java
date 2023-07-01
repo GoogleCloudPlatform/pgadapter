c3b7e79f75b315cef73088300b3455a3824f0dfe
Knut Olav Løite <koloite@gmail.com>
2023-07-01 13:52:54 +0200

    fix: remove invalid BootstrapMessage length check
    Remove the custom length check for BootstrapMessage class. This class
    marked messages with a length greater than 256 bytes as invalid, which
    is not correct.
    
    This could cause a java.lang.IllegalArgumentException: Invalid bootstrap message length: <length>
    error to be thrown.

--- src/main/java/com/google/cloud/spanner/pgadapter/wireprotocol/BootstrapMessage.java
+++ src/main/java/com/google/cloud/spanner/pgadapter/wireprotocol/BootstrapMessage.java
@@ -37,8 +37,6 @@ import java.util.Map;
  */
 @InternalApi
 public abstract class BootstrapMessage extends WireMessage {
-  private static final int MAX_BOOTSTRAP_MESSAGE_LENGTH = 1 << 8;
-
   public BootstrapMessage(ConnectionHandler connection, int length) {
     super(connection, length);
   }
@@ -53,9 +51,6 @@ public abstract class BootstrapMessage extends WireMessage {
    */
   public static BootstrapMessage create(ConnectionHandler connection) throws Exception {
     int length = connection.getConnectionMetadata().getInputStream().readInt();
-    if (length > MAX_BOOTSTRAP_MESSAGE_LENGTH) {
-      throw new IllegalArgumentException("Invalid bootstrap message length: " + length);
-    }
     int protocol = connection.getConnectionMetadata().getInputStream().readInt();
     switch (protocol) {
       case SSLMessage.IDENTIFIER:
