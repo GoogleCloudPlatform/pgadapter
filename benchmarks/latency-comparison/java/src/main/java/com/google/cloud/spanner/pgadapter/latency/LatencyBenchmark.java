f52320344788a1190621b0e1eedaae64d47212b2
Knut Olav Løite <koloite@gmail.com>
2023-08-02 13:45:46 +0200

    test: add option for storing test results

--- benchmarks/latency-comparison/java/src/main/java/com/google/cloud/spanner/pgadapter/latency/LatencyBenchmark.java
+++ benchmarks/latency-comparison/java/src/main/java/com/google/cloud/spanner/pgadapter/latency/LatencyBenchmark.java
@@ -14,8 +14,17 @@
 
 package com.google.cloud.spanner.pgadapter.latency;
 
+import com.google.cloud.spanner.DatabaseAdminClient;
+import com.google.cloud.spanner.DatabaseClient;
 import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.Mutation;
+import com.google.cloud.spanner.SessionPoolOptions;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerExceptionFactory;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.common.collect.ImmutableList;
 import java.time.Duration;
+import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -41,6 +50,12 @@ public class LatencyBenchmark {
     options.addOption("skip_pg", false, "Skip PostgreSQL JDBC benchmarks.");
     options.addOption("skip_jdbc", false, "Skip Cloud Spanner JDBC benchmarks.");
     options.addOption("skip_spanner", false, "Skip Cloud Spanner client library benchmarks.");
+    options.addOption(
+        "create_results_table",
+        false,
+        "Create the results table in the test database if it does not exist.");
+    options.addOption("s", "store_results", false, "Store results in the test database.");
+    options.addOption("name", true, "Name of this test run");
     CommandLineParser parser = new DefaultParser();
     CommandLine cmd = parser.parse(options, args);
 
@@ -59,6 +74,9 @@ public class LatencyBenchmark {
     }
 
     LatencyBenchmark benchmark = new LatencyBenchmark(DatabaseId.of(fullyQualifiedDatabase));
+    if (cmd.hasOption("create_results_table")) {
+      benchmark.createResultsTableIfNotExists();
+    }
     benchmark.run(cmd);
   }
 
@@ -69,6 +87,7 @@ public class LatencyBenchmark {
   }
 
   public void run(CommandLine commandLine) {
+    Instant runTime = Instant.now();
     int clients =
         commandLine.hasOption('c') ? Integer.parseInt(commandLine.getOptionValue('c')) : 16;
     int operations =
@@ -76,6 +95,7 @@ public class LatencyBenchmark {
     boolean runPg = !commandLine.hasOption("skip_pg");
     boolean runJdbc = !commandLine.hasOption("skip_jdbc");
     boolean runSpanner = !commandLine.hasOption("skip_spanner");
+    String name = commandLine.getOptionValue("name");
 
     System.out.println();
     System.out.println("Running benchmark with the following options");
@@ -116,9 +136,14 @@ public class LatencyBenchmark {
     printResults("PostgreSQL JDBC Driver", pgJdbcResults);
     printResults("Cloud Spanner JDBC Driver", jdbcResults);
     printResults("Java Client Library", javaClientResults);
+
+    if (commandLine.hasOption('s')) {
+      saveResults(
+          runTime, name, clients, operations, pgJdbcResults, jdbcResults, javaClientResults);
+    }
   }
 
-  public void printResults(String header, List<Duration> results) {
+  private void printResults(String header, List<Duration> results) {
     if (results == null) {
       return;
     }
@@ -128,19 +153,106 @@ public class LatencyBenchmark {
     System.out.println(header);
     System.out.printf("Total number of queries: %d\n", orderedResults.size());
     System.out.printf("Avg: %.2fms\n", avg(results));
-    System.out.printf(
-        "P50: %.2fms\n",
-        orderedResults.get(orderedResults.size() / 2).get(ChronoUnit.NANOS) / 1_000_000.0f);
-    System.out.printf(
-        "P95: %.2fms\n",
-        orderedResults.get(95 * orderedResults.size() / 100).get(ChronoUnit.NANOS) / 1_000_000.0f);
-    System.out.printf(
-        "P99: %.2fms\n",
-        orderedResults.get(99 * orderedResults.size() / 100).get(ChronoUnit.NANOS) / 1_000_000.0f);
+    System.out.printf("P50: %.2fms\n", percentile(50, orderedResults));
+    System.out.printf("P95: %.2fms\n", percentile(95, orderedResults));
+    System.out.printf("P99: %.2fms\n", percentile(99, orderedResults));
+  }
+
+  private void saveResults(
+      Instant runTime,
+      String name,
+      int clients,
+      int operations,
+      List<Duration> pgResults,
+      List<Duration> jdbcResults,
+      List<Duration> javaResults) {
+    System.out.println("Saving results to the database");
+    Mutation.WriteBuilder builder =
+        Mutation.newInsertBuilder("latency_test_results")
+            .set("run_time")
+            .to(runTime.toString())
+            .set("name")
+            .to(name)
+            .set("clients")
+            .to(clients)
+            .set("operations")
+            .to(operations);
+    builder = addResults("pg", pgResults, builder);
+    builder = addResults("jdbc", jdbcResults, builder);
+    builder = addResults("java", javaResults, builder);
+
+    try (Spanner spanner = createSpanner()) {
+      DatabaseClient client = spanner.getDatabaseClient(databaseId);
+      client.write(ImmutableList.of(builder.build()));
+    }
+  }
+
+  private Mutation.WriteBuilder addResults(
+      String prefix, List<Duration> results, Mutation.WriteBuilder builder) {
+    if (results == null) {
+      return builder;
+    }
+    return builder
+        .set(prefix + "_avg")
+        .to(avg(results))
+        .set(prefix + "_p50")
+        .to(percentile(50, results))
+        .set(prefix + "_p95")
+        .to(percentile(95, results))
+        .set(prefix + "_p99")
+        .to(percentile(99, results));
+  }
+
+  private double percentile(int percentile, List<Duration> orderedResults) {
+    return orderedResults.get(percentile * orderedResults.size() / 100).get(ChronoUnit.NANOS)
+        / 1_000_000.0f;
   }
 
   private double avg(List<Duration> results) {
     return results.stream()
         .collect(Collectors.averagingDouble(result -> result.get(ChronoUnit.NANOS) / 1_000_000.0f));
+  }
+
+  private Spanner createSpanner() {
+    return SpannerOptions.newBuilder()
+        .setProjectId(databaseId.getInstanceId().getProject())
+        .setSessionPoolOption(
+            SessionPoolOptions.newBuilder().setMinSessions(1).setMaxSessions(10).build())
+        .build()
+        .getService();
+  }
+
+  private void createResultsTableIfNotExists() {
+    System.out.println("Creating results table if it does not already exist");
+    try (Spanner spanner = createSpanner()) {
+      DatabaseAdminClient client = spanner.getDatabaseAdminClient();
+      client
+          .updateDatabaseDdl(
+              databaseId.getInstanceId().getInstance(),
+              databaseId.getDatabase(),
+              ImmutableList.of(
+                  "create table if not exists latency_test_results (\n"
+                      + "run_time varchar primary key,\n"
+                      + "name varchar,\n"
+                      + "clients bigint,\n"
+                      + "operations bigint,\n"
+                      + "pg_avg float8,\n"
+                      + "pg_p50 float8,\n"
+                      + "pg_p95 float8,\n"
+                      + "pg_p99 float8,\n"
+                      + "jdbc_avg float8,\n"
+                      + "jdbc_p50 float8,\n"
+                      + "jdbc_p95 float8,\n"
+                      + "jdbc_p99 float8,\n"
+                      + "java_avg float8,\n"
+                      + "java_p50 float8,\n"
+                      + "java_p95 float8,\n"
+                      + "java_p99 float8\n"
+                      + ")"),
+              null)
+          .get();
+    } catch (Throwable t) {
+      throw SpannerExceptionFactory.asSpannerException(t);
+    }
   }
 }
