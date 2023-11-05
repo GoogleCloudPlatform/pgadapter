c66bbee1bac59ce19bb6e05bf491dd6e2fb1243d
Knut Olav Løite <koloite@gmail.com>
2023-11-05 20:27:26 +0100

    feat: support knex

---
+++ src/test/java/com/google/cloud/spanner/pgadapter/nodejs/KnexMockServerTest.java
@@ -0,0 +1,135 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.spanner.pgadapter.nodejs;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.google.cloud.spanner.MockSpannerServiceImpl.StatementResult;
+import com.google.cloud.spanner.Statement;
+import com.google.cloud.spanner.pgadapter.AbstractMockServerTest;
+import com.google.cloud.spanner.pgadapter.metadata.OptionsMetadata;
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.ListValue;
+import com.google.protobuf.Value;
+import com.google.spanner.v1.ExecuteSqlRequest;
+import com.google.spanner.v1.ExecuteSqlRequest.QueryMode;
+import com.google.spanner.v1.ResultSet;
+import com.google.spanner.v1.ResultSetMetadata;
+import com.google.spanner.v1.TypeCode;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@Category(NodeJSTest.class)
+@RunWith(Parameterized.class)
+public class KnexMockServerTest extends AbstractMockServerTest {
+  @Parameter public boolean useDomainSocket;
+
+  @Parameters(name = "useDomainSocket = {0}")
+  public static Object[] data() {
+    OptionsMetadata options = new OptionsMetadata(new String[] {"-p p", "-i i"});
+    return options.isDomainSocketEnabled() ? new Object[] {true, false} : new Object[] {false};
+  }
+
+  @BeforeClass
+  public static void installDependencies() throws IOException, InterruptedException {
+    NodeJSTest.installDependencies("node-postgres");
+  }
+
+  private String getHost() {
+    if (useDomainSocket) {
+      return "/tmp";
+    }
+    return "localhost";
+  }
+
+  @Test
+  public void testSelect1() throws Exception {
+    String sql = "select * from (select 1)";
+    mockSpanner.putStatementResult(StatementResult.query(Statement.of(sql), SELECT1_RESULTSET));
+
+    String output = runTest("testSelect1", getHost(), pgServer.getLocalPort());
+
+    assertEquals("SELECT 1 returned: 1\n", output);
+
+    List<ExecuteSqlRequest> executeSqlRequests =
+        mockSpanner.getRequestsOfType(ExecuteSqlRequest.class).stream()
+            .filter(request -> request.getSql().equals(sql))
+            .collect(Collectors.toList());
+    assertEquals(1, executeSqlRequests.size());
+    ExecuteSqlRequest request = executeSqlRequests.get(0);
+    assertTrue(request.getTransaction().hasSingleUse());
+    assertTrue(request.getTransaction().getSingleUse().hasReadOnly());
+  }
+
+  @Test
+  public void testSelectUser() throws Exception {
+    String sql = "select * from \"users\" where \"id\" = $1 limit $2";
+    ResultSetMetadata metadata =
+        createMetadata(
+                ImmutableList.of(TypeCode.INT64, TypeCode.STRING), ImmutableList.of("id", "name"))
+            .toBuilder()
+            .setUndeclaredParameters(
+                createParameterTypesMetadata(ImmutableList.of(TypeCode.INT64, TypeCode.INT64))
+                    .getUndeclaredParameters())
+            .build();
+    mockSpanner.putStatementResult(
+        StatementResult.query(
+            Statement.of(sql), ResultSet.newBuilder().setMetadata(metadata).build()));
+    mockSpanner.putStatementResult(
+        StatementResult.query(
+            Statement.newBuilder(sql).bind("p1").to(1L).bind("p2").to(1L).build(),
+            ResultSet.newBuilder()
+                .setMetadata(metadata)
+                .addRows(
+                    ListValue.newBuilder()
+                        .addValues(Value.newBuilder().setStringValue("1").build())
+                        .addValues(Value.newBuilder().setStringValue("User 1"))
+                        .build())
+                .build()));
+
+    String output = runTest("testSelectUser", getHost(), pgServer.getLocalPort());
+
+    assertEquals("{ id: '1', name: 'User 1' }\n", output);
+
+    List<ExecuteSqlRequest> executeSqlRequests =
+        mockSpanner.getRequestsOfType(ExecuteSqlRequest.class).stream()
+            .filter(request -> request.getSql().equals(sql))
+            .collect(Collectors.toList());
+    // Knex uses the extended protocol and describes the statement first.
+    assertEquals(2, executeSqlRequests.size());
+    ExecuteSqlRequest planRequest = executeSqlRequests.get(0);
+    assertEquals(QueryMode.PLAN, planRequest.getQueryMode());
+    assertTrue(planRequest.getTransaction().hasSingleUse());
+    assertTrue(planRequest.getTransaction().getSingleUse().hasReadOnly());
+    ExecuteSqlRequest executeRequest = executeSqlRequests.get(1);
+    assertEquals(QueryMode.NORMAL, executeRequest.getQueryMode());
+    assertTrue(executeRequest.getTransaction().hasSingleUse());
+    assertTrue(executeRequest.getTransaction().getSingleUse().hasReadOnly());
+  }
+
+  static String runTest(String testName, String host, int port)
+      throws IOException, InterruptedException {
+    return NodeJSTest.runTest("knex-tests", testName, host, port, "db");
+  }
+}
