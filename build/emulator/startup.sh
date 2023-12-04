421aabca9af0522c1f987a963c5139baa3d875d0
Knut Olav Løite <koloite@gmail.com>
2023-12-04 08:44:00 +0100

    feat: build Docker image with PGAdapter+Emulator
    Adds a Docker image to the build that contains both PGAdapter and the
    Cloud Spanner Emulator. Running this Docker image will create a container
    that starts both PGAdapter and the Emulator, and that automatically
    configures PGAdapter to use the emulator in the Docker container.
    
    The PGAdapter instance will accept connections for any valid database
    name. If the database does not exist, it will automatically be created
    on the emulator.

---
+++ build/emulator/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ARGUMENTS="-p emulator-project -i test-instance -r autoConfigEmulator=true -c \"\" -x"
+JAVA_ARGUMENTS=""
+
+for var in "$@"
+do
+  if [[ $var == "-D"* ]]; then
+    JAVA_ARGUMENTS="${JAVA_ARGUMENTS} $var"
+  else
+    ARGUMENTS="${ARGUMENTS} $var"
+  fi
+done
+
+nohup sh -c "/emulator/emulator_main --host_port localhost:9010 &"
+
+cd /home/pgadapter
+COMMAND="java ${JAVA_ARGUMENTS} -jar pgadapter.jar ${ARGUMENTS}"
+echo $COMMAND
+exec $COMMAND
