b67b03d4f05a48193c600c541683d3f716f62916
Knut Olav Løite <koloite@gmail.com>
2024-06-08 07:44:09 +0200

    docs: enable pooling for dotnet samples (#1941)
    Pooling was disabled for dotnet samples due to lack of support for
    DISCARD ALL. This has now been added and released.

--- samples/snippets/dotnet-snippets/WriteDataWithCopy.cs
+++ samples/snippets/dotnet-snippets/WriteDataWithCopy.cs
@@ -21,7 +21,7 @@ public static class WriteDataWithCopySample
 {
     public static void WriteDataWithCopy(string host, int port, string database)
     {
-        var connectionString = $"Host={host};Port={port};Database={database};SSL Mode=Disable;Pooling=False";
+        var connectionString = $"Host={host};Port={port};Database={database};SSL Mode=Disable";
         using var connection = new NpgsqlConnection(connectionString);
         connection.Open();
         
