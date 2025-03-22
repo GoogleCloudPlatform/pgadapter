ab35b282f8f1dbcba804a9a7d6da008476e5949f
Knut Olav Løite <koloite@gmail.com>
2025-03-22 10:55:03 +0100

    chore: experiment with native executable

--- build/native-image/build.sh
+++ build/native-image/build.sh
@@ -2,9 +2,11 @@ mvn clean
 mvn package -P assembly -DskipTests
 cd target/pgadapter
 native-image \
-  --shared \
   --initialize-at-build-time=com.google.protobuf,com.google.gson \
-  -J-Xmx14g -H:IncludeResources=".*metadata.*json$" \
+  -J-Xmx14g \
+  -H:IncludeResources=".*" \
   -H:ReflectionConfigurationFiles=../../build/native-image/reflectconfig.json \
   -jar pgadapter.jar \
   --no-fallback
+
+  -H:IncludeResources=".*metadata.*json$" \
