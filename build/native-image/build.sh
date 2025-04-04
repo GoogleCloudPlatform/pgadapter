3ac701c6035d8adb763f078c8ffac93dc11f0f53
Knut Olav Løite <koloite@gmail.com>
2025-04-04 11:02:29 +0200

    build: update build script

--- build/native-image/build.sh
+++ build/native-image/build.sh
@@ -1,12 +1,12 @@
-mvn clean
-mvn package -P assembly -DskipTests
+mvn clean package -P assembly -DskipTests
 cd target/pgadapter
 native-image \
-  --initialize-at-build-time=com.google.protobuf,com.google.gson \
+  --initialize-at-build-time=com.google.protobuf,com.google.gson,com.google.cloud.spanner.pgadapter.Server \
   -J-Xmx14g \
   -H:IncludeResources=".*" \
   -H:ReflectionConfigurationFiles=../../build/native-image/reflectconfig.json \
   -jar pgadapter.jar \
   --no-fallback
+  -H:IncludeResources=".*metadata.*json$"
 
-  -H:IncludeResources=".*metadata.*json$" \
+./pgadapter -p appdev-soda-spanner-staging -i knut-test-ycsb -s 5433
