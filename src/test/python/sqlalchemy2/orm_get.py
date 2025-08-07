d05ec637e716c348544e35951b104db4545f78ea
Sakthivel Subramanian <sakthipvmj@gmail.com>
2025-08-07 12:30:45 +0530

    Remove non-java tests

--- src/test/python/sqlalchemy2/orm_get.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from connect import create_test_engine
-from all_types import AllTypes
-
-
-engine = create_test_engine(options="?options=-c timezone=UTC")
-session = Session(engine)
-row = session.get(AllTypes, 1)
-print(row)
