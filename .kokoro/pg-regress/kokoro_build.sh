2e5d8c524a832c639eb1c3c3efb45b828a4424b6
Hengfeng Li‬ <hengfeng@google.com>
2026-02-04 09:41:23 +0000

    recover to the previous solution

--- .kokoro/pg-regress/kokoro_build.sh
+++ .kokoro/pg-regress/kokoro_build.sh
@@ -72,10 +72,10 @@ python --version
 pip install google-cloud-bigquery
 
 # Install psql
-sudo apt install -y postgresql-common
-sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install --yes --no-install-recommends postgresql-client-16
+sudo apt-get install --yes --no-install-recommends postgresql-client-17
 psql --version
 
 # Get postgresql source code
