1102995000eecb36bb930973a02df78e19aa6c33
Hengfeng Li‬ <hengfeng@google.com>
2026-02-04 04:58:51 +0000

    attempt to fix build issue

--- .kokoro/pg-regress/kokoro_build.sh
+++ .kokoro/pg-regress/kokoro_build.sh
@@ -71,7 +71,7 @@ python --version
 pip install google-cloud-bigquery
 
 # Install psql
-sudo apt-get install --yes --no-install-recommends postgresql-client
+sudo apt-get install --yes --no-install-recommends postgresql-client-17
 psql --version
 
 # Get postgresql source code
