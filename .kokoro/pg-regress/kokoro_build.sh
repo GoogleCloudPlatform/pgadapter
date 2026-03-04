5318e4248c2e1d4e2f065f3bf278def3e0728802
Hengfeng Li‬ <hengfeng@google.com>
2026-03-04 22:04:46 +0000

    use a different command

--- .kokoro/pg-regress/kokoro_build.sh
+++ .kokoro/pg-regress/kokoro_build.sh
@@ -118,5 +118,5 @@ ts=$(date +%s)
 #python upload_bigquery.py results.json $BQ_TABLE $TARGET_ENV $ts
 #gcloud storage cp results.json $GCS_BUCKET_PATH/results_$ts.json
 #gcloud storage cp regression.diffs $GCS_BUCKET_PATH/regression_$ts.diffs
-gcloud storage cp results/*.out "${GCS_BUCKET_PATH}/lastest-run-results/"
+gcloud storage cp -r results/ "${GCS_BUCKET_PATH}/lastest-run-results/"
 
