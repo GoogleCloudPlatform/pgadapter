45bae952ce7961ca117f83688b1ef63b0c0e33f4
Hengfeng Li‬ <hengfeng@google.com>
2026-03-17 04:55:25 +0000

    try to fix group not found issue

--- .kokoro/pg-regress/kokoro_build.sh
+++ .kokoro/pg-regress/kokoro_build.sh
@@ -39,7 +39,7 @@ elif [[ "$SPANNER_ENV" == "internal-emulator" ]]; then
   # Run the emulator
   ls -l ${KOKORO_BLAZE_DIR}/internal_emulator/blaze-bin/third_party/cloud_spanner_emulator/binaries/
   ls -lR "${KOKORO_BLAZE_DIR}/internal_emulator/blaze-bin/third_party/cloud_spanner_emulator/binaries/gateway_main.runfiles"
-  ${KOKORO_BLAZE_DIR}/internal_emulator/blaze-bin/third_party/cloud_spanner_emulator/binaries/gateway_main 2>&1 &
+  ${KOKORO_BLAZE_DIR}/internal_emulator/blaze-bin/third_party/cloud_spanner_emulator/binaries/gateway_main --uid= --gid= 2>&1 &
 
   # Config gcloud to emulator
   gcloud config configurations create emulator
